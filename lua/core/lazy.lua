89e788b92fe77bf32623351fff6552de9737a290
Michell Stuttgart <michellstut@gmail.com>
2024-05-07 12:54:55 -0300

    Add new options do lazy

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -1,16 +1,74 @@
+-- Install lazy.nvim automatically
+
+local opts = {
+  ui = {
+    custom_keys = { false },
+  },
+  install = {
+    -- try to load one of these colorschemes when starting an installation during startup
+    colorscheme = { "everforest" },
+    -- install missing plugins on startup. This doesn't increase startup time.
+    missing = true,
+  },
+  defaults = {
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "2html_plugin",
+        "bugreport",
+        "compiler",
+        "ftplugin",
+        "getscript",
+        "getscriptPlugin",
+        "gzip",
+        "logipat",
+        "matchit",
+        "netrw",
+        "netrwFileHandlers",
+        "netrwPlugin",
+        "netrwSettings",
+        "optwin",
+        "rplugin",
+        "rrhelper",
+        "spellfile_plugin",
+        "synmenu",
+        "syntax",
+        "tar",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "vimball",
+        "vimballPlugin",
+        "zip",
+        "zipPlugin",
+      },
+    },
+  },
+  checker = {
+    enabled = true,
+  },
+  debug = false,
+}
+
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
+if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
     "git",
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
     lazypath,
   }
 end
 
-vim.opt.rtp:prepend(lazypath)
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
-require("lazy").setup "plugins"
+-- -- Load the plugins and options
+require("lazy").setup("plugins", opts)
