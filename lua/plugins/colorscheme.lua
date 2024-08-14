ba92a3ca413039cfe0dddc6c898c009755336429
Michell Stuttgart <michellstut@gmail.com>
2024-08-14 15:52:10 -0300

    Remove colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,15 +2,6 @@
 
 local plugins = {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = true,
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme "catppuccin"
-    end,
-  },
-  {
     "neanias/everforest-nvim",
     name = "everforest",
     lazy = true,
@@ -27,16 +18,6 @@ local plugins = {
       }
       -- vim.o.background = "light"
       vim.cmd.colorscheme "everforest"
-    end,
-  },
-  {
-    "sainnhe/gruvbox-material",
-    name = "gruvbox-material",
-    lazy = true,
-    priority = 1000,
-    config = function()
-      vim.g.gruvbox_material_background = "hard"
-      vim.cmd.colorscheme "gruvbox-material"
     end,
   },
   {
