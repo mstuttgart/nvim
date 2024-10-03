713667b7be59b226530191b9206796d5ec14486b
Michell Stuttgart <michellstut@gmail.com>
2024-10-03 09:20:51 -0300

    Remove unused colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -21,37 +21,6 @@ local plugins = {
     end,
   },
   {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    lazy = true,
-    priority = 1000,
-    config = function()
-      require("rose-pine").setup {
-        variant = "auto", -- auto, main, moon, or dawn
-        dark_variant = "main", -- main, moon, or dawn
-        dim_inactive_windows = false,
-        extend_background_behind_borders = true,
-
-        enable = {
-          terminal = true,
-          legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-          migrations = true, -- Handle deprecated options automatically
-        },
-
-        styles = {
-          bold = true,
-          italic = true,
-          transparency = false,
-        },
-      }
-
-      vim.cmd "colorscheme rose-pine"
-      -- vim.cmd("colorscheme rose-pine-main")
-      -- vim.cmd("colorscheme rose-pine-moon")
-      -- vim.cmd("colorscheme rose-pine-dawn")
-    end,
-  },
-  {
     "shaunsingh/nord.nvim",
     name = "nord",
     lazy = true,
