8ed527cf5d59db7b009199b0bf667613f9bb2d2c
Michell Stuttgart <michellstut@gmail.com>
2024-10-03 09:20:33 -0300

    Update autosession to nvim 10

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -7,8 +7,8 @@ local plugin = {
 
     require("auto-session").setup {
       log_level = "error",
-      auto_restore_enabled = true,
-      auto_session_suppress_dirs = { "~/", "~/.config", "~/Downloads", "~/Documents" },
+      auto_restore = true,
+      suppressed_dirs = { "~/", "~/.config", "~/Downloads", "~/Documents" },
       cwd_change_handling = {
         post_cwd_changed_hook = function() -- example refreshing the lualine status line _after_ the cwd changes
           require("lualine").refresh() -- refresh lualine so the new session name is displayed in the status bar
