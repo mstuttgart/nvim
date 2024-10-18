01c7d346d21127b0555a3c123c8c7be74c5c4a15
Michell Stuttgart <michellstut@gmail.com>
2024-10-18 17:04:52 -0300

    Fix LSP config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,21 +2,19 @@
 
 local plugin = {
   "neovim/nvim-lspconfig",
-  ---@class PluginLspOpts
   opts = {
-    ---@type lspconfig.options
     servers = {
-      "html",
-      "cssls",
-      "pyright",
-      "bashls",
-      "ansiblels",
-      "lua_ls",
-      "lemminx",
-      "taplo",
-      "dockerls",
-      "docker_compose_language_service",
-      "vuels",
+      html = {},
+      cssls = {},
+      pyright = {},
+      bashls = {},
+      ansiblels = {},
+      lua_ls = {},
+      lemminx = {},
+      taplo = {},
+      dockerls = {},
+      docker_compose_language_service = {},
+      vuels = {},
     },
   },
 }
