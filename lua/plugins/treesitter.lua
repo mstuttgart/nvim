22f87dcfa66e70dd2c35ab3f3485c4176b3b42e4
Michell Stuttgart <michellstut@gmail.com>
2024-10-18 15:05:30 -0300

    Adjust treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -33,11 +33,6 @@ local plugin = {
         "xml",
         "yaml",
       },
-      query_linter = {
-        enable = true,
-        use_virtual_text = true,
-        lint_events = { "BufWrite", "CursorHold" },
-      },
     },
   },
 }
